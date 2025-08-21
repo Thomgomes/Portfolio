cc95b3fc35bee98f6db22f8ffe5b31867c59f311
ThomGomes <math.011@hotmail.com>
2025-08-20 23:44:41 -0300

    feat: add i18n

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,6 +1,12 @@
+import { useTranslations } from "next-intl";
+
 export default function Home() {
+  const { t } = useTranslations("Home");
+
   return (
     <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <p>teste</p>
+      <p>{t('title')}</p>
     </div>
   );
 }
