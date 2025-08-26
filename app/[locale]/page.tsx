b52ea3bbe309b1e14167cf587e137836d51cce0f
ThomGomes <math.011@hotmail.com>
2025-08-25 22:19:23 -0300

    fix

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,14 +3,14 @@ import { useTranslations } from "next-intl";
 
 export default function HomePage() {
 
-  const t = useTranslations('home');
+  const t = useTranslations('');
 
   return (
     <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <p>{t('greeting')}</p>
-      <h1>{t('title')}</h1>
-      <h2>{t('subtitle')}</h2>
-      <p>{t('description')}</p>
+      <p>{t('home.greeting')}</p>
+      <h1>{t('home.title')}</h1>
+      <h2>{t('home.subtitle')}</h2>
+      <p>{t('about.title')}</p>
 
     </div>
   );
