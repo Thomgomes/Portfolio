6bf8ce227437b0def9e39bfcd41274ea57c00812
ThomGomes <math.011@hotmail.com>
2025-09-04 11:13:10 -0300

    fix: home

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,29 +2,9 @@ import { useTranslations } from "next-intl";
 
 import Header from "@/components/layout/header";
 import { Line } from "@/components/line";
-import Pray from "@/components/pray";
 import ProjectShowcase from "@/components/project-showcase";
-import { ThemeToggleButton } from "@/components/theme-toggle-button";
-import TitleGradient from "@/components/title-gradient";
 import ParticleBackground from "@/components/particle-background";
 import { ArrowBigDownDash } from "lucide-react";
-
-const ArrowIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor"
-    className="w-5 h-5 transition-transform duration-300 group-hover:translate-x-0.5"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-    />
-  </svg>
-);
 
 export default function HomePage() {
   const t = useTranslations('home');
