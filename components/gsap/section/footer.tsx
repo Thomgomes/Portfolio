27d675cc41a39797f36e2a16902604fcdd8ee48b
ThomGomes <math.011@hotmail.com>
2025-10-13 19:01:45 -0300

    fix: footer

--- components/gsap/section/footer.tsx
+++ components/gsap/section/footer.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Github, Linkedin, Twitter, Mail, ArrowUp } from 'lucide-react';
+import { Github, Linkedin, Twitter, Mail, ArrowUp, Instagram } from 'lucide-react';
 
 export default function Footer() {
   const scrollToTop = () => {
@@ -10,16 +10,19 @@ export default function Footer() {
     <footer className="py-8 sm:py-12 px-4 sm:px-6 lg:px-8 border-t border-border bg-background-alt">
       <div className="max-w-7xl mx-auto">
         {/* Grid principal */}
-        <div className="flex justify-between mb-8">
+        <div className="flex flex-col md:flex-row items-center gap-6 md:gap-0 md:justify-between  mb-8">
           {/* Coluna 1 - Nome e descrição */}
           <div>
+            <div className='hidden md:block'>
+
             <h3 className="text-xl sm:text-2xl font-bold mb-2 sm:mb-3 text-foreground">
               Thom Gomes
             </h3>
             <p className="text-sm sm:text-base text-muted-foreground mb-4">
               Desenvolvedor Frontend e ...
             </p>
-            <div className="flex gap-4">
+            </div>
+            <div className="flex gap-16 md:gap-4">
               <a 
                 href="#" 
                 className="text-secondary hover:text-primary transition-colors duration-300"
@@ -39,20 +42,13 @@ export default function Footer() {
                 className="text-secondary hover:text-pray transition-colors duration-300"
                 aria-label="Twitter"
               >
-                <Twitter className="w-5 h-5 sm:w-6 sm:h-6" />
-              </a>
-              <a 
-                href="#" 
-                className="text-secondary hover:text-primary transition-colors duration-300"
-                aria-label="Email"
-              >
-                <Mail className="w-5 h-5 sm:w-6 sm:h-6" />
+                <Instagram className="w-5 h-5 sm:w-6 sm:h-6" />
               </a>
             </div>
           </div>
 
           {/* Coluna 3 - Contato */}
-          <div>
+          <div className='flex flex-col items-center md:items-start'>
             <h4 className="text-base sm:text-lg font-semibold mb-3 sm:mb-4 text-foreground">
               Contato
             </h4>
