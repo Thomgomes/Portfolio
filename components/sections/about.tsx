f830ae5c9bfe43f02347ed48bbcf6d6dce55ed31
ThomGomes <math.011@hotmail.com>
2025-10-26 01:01:16 -0300

    fix

--- components/sections/about.tsx
+++ components/sections/about.tsx
@@ -114,7 +114,7 @@ export default function AboutSection() {
           </motion.div>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6">
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6">
           {highlights.map((item, index) => (
             <motion.div
               key={item.key}
